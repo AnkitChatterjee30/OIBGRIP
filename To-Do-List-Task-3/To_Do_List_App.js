9d4a7ad2b046d40217873c6d43674a7ad1a448a0
ANKIT CHATTERJEE <91140563+AnkitChatterjee30@users.noreply.github.com>
2022-08-06 12:02:06 +0530

    Add files via upload

---
+++ To-Do-List-Task-3/To_Do_List_App.js
@@ -0,0 +1,23 @@
+let music = new Audio("Click.mp3");
+let bgm = new Audio("Switch-click-sound.mp3");
+$(".text-box").on("keyup", function (e) {
+    if (e.keyCode == 13 && $(".text-box").val() != "") {
+        var task = $("<div class='task'></div>").text($(".text-box").val());
+        var del = $("<i class='fas fa-trash-alt'></i>").click(function () {
+            var p = $(this).parent(); p.fadeOut(function () {
+                p.remove();
+            });
+        });
+        var check = $("<i class='fas fa-check'></i>").click(function () {
+            var p = $(this).parent(); p.fadeOut(function () { $(".completed").append(p); p.fadeIn(); });
+            $(this).remove();
+        }); task.append(del, check);
+        music.play();
+        $(".notcomp").append(task); $(".text-box").val("");
+    }
+});
+function darkmode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+    bgm.play();
+};
